73cb676533036ce9945a2901a6964757ff2d2082
Mikhail.H <m.haidayenka@yandex.by>
2019-09-24 21:24:18 +0300

    delete visitsite from this directory

--- Django_progects/VisitSite/VisitSite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('myadmin/', admin.site.urls),
-    path('', include('visit.urls'))
-]
--- Django_progects/VisitSite/visit/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from visit.models import Skills
-
-admin.site.register(Skills)
